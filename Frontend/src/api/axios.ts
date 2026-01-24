b1703ac5c9e238c1fc2d0d5f22041d625a75a0c3
Divine <irasubizadivine009@gmail.com>
2026-01-24 18:25:38 +0200

    Update

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://agaciro.onrender.com",
+    baseURL: import.meta.env.PROD 
+        ? "https://agaciro.onrender.com" 
+        : "http://localhost:3000",
 });
 
 api.interceptors.request.use((config) => {
