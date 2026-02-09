43ba5342dec53e5c21ccab9a953a0c18638d5797
Divine <irasubizadivine009@gmail.com>
2026-02-09 10:42:41 +0200

    Josh debug

--- Frontend/src/components/ProtectedRoute.tsx
+++ Frontend/src/components/ProtectedRoute.tsx
@@ -1,21 +1,24 @@
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import type { JSX } from "react/jsx-dev-runtime";
 
 type Props = {
-  children: React.ReactElement;
+  children: JSX.Element;
   role?: "admin" | "customer";
 };
 
-export default function ProtectedRoute({ children, role }: Props) {
-  const token = localStorage.getItem("token");
-  const user = JSON.parse(localStorage.getItem("user") || "null");
+export default function ProtectedRoute({ children, 
+  role 
+}: Props) {
+  const { user } = useAuth();
 
-  // if (!token || !user) {
-  //   return <Navigate to="/login" replace />;
-  // }
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
 
-  // if (role && user.role !== role) {
-  //   return <Navigate to="/" replace />;
-  // }
+  if (role && user.role !== role) {
+    return <Navigate to="/" replace />;
+  }
 
   return children;
 }
